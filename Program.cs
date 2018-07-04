1a26cefb4e14c7e2d8349ce1d8827a887eb28290
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-04 17:21:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Text;
+
+
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Document");
+
+            Console.WriteLine("Enter Document name: ");
+            String filename = Console.ReadLine();
+
+            Console.WriteLine("Enter Contents: ");
+            String contents = Console.ReadLine();
+
+
+            String newFileName = filename + ".txt";
+
+            Console.WriteLine(newFileName); 
+
+    
+            String line;
+            try
+            {
+                
+                //Pass the file path and file name to the StreamWriter constructor
+                StreamWriter sw = new StreamWriter(newFileName);
+
+
+                // Write the first line of text
+                sw.Write(contents);
+                sw.Write("\n");
+
+                //close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+
+            }
+
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+   
+
+        }
+    }
+}
